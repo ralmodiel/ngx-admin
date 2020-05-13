d726dfb7413cfb1b21b717a5f2c41d1400a2075b
Roque Almodiel Jr <roque.almodiel@gmail.com>
2020-05-13 20:08:31 +0800

    Fixed errors post-merge.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -27,7 +27,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/proposals/reflect-metadata';
+import 'core-js/es/reflect';
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
